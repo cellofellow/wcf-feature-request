5ffca36e2056b997b36c5b81dc9532de61d04992
Joshua Gardner <mellowcellofellow@gmail.com>
2018-08-18 21:33:34 -0600

    Add flask-restful and marshmallow API basics

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import typing as t
+
+from flask import Flask, request, url_for, jsonify
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config.from_object('default_settings')
-db = SQLAlchemy(app)
 
+
+@app.route('/')
+def root():
+    return '<h1>Hello World</h1>'
+
+
+# SQLAlchemy Models
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
 
 class Client(db.Model):
     __tablename__ = 'client'
@@ -71,7 +81,57 @@ class FeatureRequest(db.Model):
         db.UniqueConstraint('client_id', 'priority'),
     )
 
+    @property
+    def id(self) -> int:
+        return self.featurerequest_id
 
+    def handle_unique_constraints(self) -> t.Optional[str]:
+        '''
+        Each FeatureRequest must have a unique priority number for each client.
+        When creating or updating a request, use this method to manage the
+        conflict when a request with an existing priority is created. This
+        increments the priority of all other request for that client equal to
+        or above the new request's priority.
+
+        Just return an error if any other unique constraints (title) are
+        violated.
+        '''
+        cls = type(self)
+        db.session.add(self)
+        try:
+            db.session.flush()
+        except IntegrityError as err:
+            reason = err.args[0]
+            is_uniq = 'UNIQUE constraint failed: ' in reason
+            if not is_uniq:
+                raise
+            is_priority_client = (
+                '.client_id' in reason
+                and '.priority' in reason
+            )
+            if is_priority_client:
+                db.session.rollback()
+                (db.session.query(FeatureRequest)
+                 .filter(cls.client_id == self.client_id)
+                 .filter(cls.priority >= self.priority)
+                 .update({cls.priority: cls.priority + 1}))
+            if '.title' in reason:
+                return 'Unique title required'
+
+    def save(self) -> t.Tuple[dict, int]:
+        err = self.handle_unique_constraints()
+        if err is not None:
+            db.session.rollback()
+            return {'errors': [err]}, 409
+
+        db.session.commit()
+        import schema
+        self = self.query.get(self.id)
+        result = schema.FeatureRequestSchema().dump(self)
+        return {'featurerequest': result}, 200
+
+
+# SQLAlchemy Commands
 @app.cli.command('create-all')
 def create_all():
     db.create_all()
@@ -87,6 +147,105 @@ def default_data():
     session.commit()
 
 
+__all__ = ['Client', 'ProductArea', 'FeatureRequest']
+
 @app.cli.command('drop-all')
 def drop_all():
     db.drop_all()
+
+
+
+# Define Flask-Restful Resources
+# These use the Marshmallow schemas
+
+from marshmallow import ValidationError
+from schema import FeatureRequestSchema
+from flask_restful import Api, Resource
+
+
+api = Api(app)
+
+class FeatureRequestListResource(Resource):
+    '''
+    API resource create and list FeatureRequests
+    '''
+    schema = FeatureRequestSchema
+
+    def get(self):
+        reqs = self.schema(many=True).dump(FeatureRequest.query.all())
+        return {'featurerequests': reqs}
+
+    def post(self):
+        json_data = request.get_json()
+        if not json_data:
+            return {'message': 'Empty input received'}, 400
+        json_data.pop('id', None)
+        try:
+            req = self.schema().load(json_data)
+        except ValidationError as err:
+            return {'errors': err.messages}, 422
+        response, status = req.save()
+        if status != 200:
+            return response, status
+        response['message'] = 'Created a new FeatureRequest'
+        return response, 201
+api.add_resource(FeatureRequestListResource,
+                 '/v1/featurerequest')
+
+
+class FeatureRequestResource(Resource):
+    '''
+    API resource to read, update, and delete a FeatureRequest
+    '''
+    schema = FeatureRequestSchema
+
+    def get(self, pk):
+        req = FeatureRequest.query.get(pk)
+        if req is None:
+            return {'message': f'FeatureRequest {pk} could not be found.'}, 404
+        req_data = self.schema().dump(req)
+        return {'featurerequest': req_data}, 200
+
+    def put(self, pk, partial=False):
+        req = FeatureRequest.query.get(pk)
+        if req is None:
+            return {'message': f'FeatureRequest {pk} could not be found.'}, 404
+        json_data = request.get_json()
+        json_data['id'] = pk
+        try:
+            req = self.schema().load(json_data, partial=partial)
+        except ValidationError as err:
+            return {'errors': err.messages}, 422
+        response, status = req.save()
+        if status != 200:
+            return response, status
+        response['message'] = 'Modified FeatureRequest'
+        return response, status
+
+    def patch(self, pk):
+        return self.put(pk, partial=True)
+api.add_resource(FeatureRequestResource,
+                 '/v1/featurerequest/<pk>')
+
+
+# No need for "resources" for simple read-only listings.
+@app.route('/v1/productarea')
+def list_productareas():
+    areas = ProductArea.query.all()
+    return {'productareas': [{'id': a.id, 'name': a.name} for a in areas]}
+
+
+@app.route('/v1/client')
+def list_clients():
+    clients = Client.query.all()
+    return {'clients': [{'id': c.id, 'name': c.name} for c in clients]}
+
+
+# Simple root for the API allowing some kind of discoverability.
+@app.route('/v1')
+def api_root():
+    return jsonify({'resources': [
+        url_for('featurerequestlistresource', _external=True),
+        url_for('list_productareas', _external=True),
+        url_for('list_clients', _external=True),
+    ]})
---
+++ schema.py
@@ -0,0 +1,50 @@
+from flask import url_for
+from marshmallow import Schema, fields, post_load, validates
+
+
+class FeatureRequestSchema(Schema):
+    id = fields.Int()
+    url = fields.Method(serialize='resource_url')
+    title = fields.Str(required=True)
+    description = fields.Str(required=True)
+    priority = fields.Int(required=True, validate=lambda v: v > 0)
+    target_date = fields.Date(allow_none=True)
+    client_id = fields.Int(required=True)
+    client_name = fields.Str(attribute='client.name', dump_only=True)
+    productarea_id = fields.Int(required=True)
+    productarea_name = fields.Str(attribute='productarea.name', dump_only=True)
+
+    @validates('client_id')
+    def _client_id_exists(self, value):
+        from app import db
+        from model import Client
+        q = (Client.query
+             .filter(Client.client_id == value)
+             .exists())
+        return db.session.query(q).scalar()
+
+    @validates('productarea_id')
+    def _productarea_id_exists(self, value):
+        from app import db, ProductArea
+        q = (ProductArea.query
+             .filter(ProductArea.productarea_id == value)
+             .exists())
+        return db.session.query(q).scalar()
+
+    @post_load
+    def make_orm_object(self, data):
+        from app import FeatureRequest
+        id = data.pop('id', None)
+        if id:
+            req = FeatureRequest.query.get(id)
+            if req:
+                for k, v in data.items():
+                    setattr(req, k, v)
+        else:
+            req = FeatureRequest(**data)
+        return req
+
+    def resource_url(self, obj):
+        return url_for('featurerequestresource',
+                       pk=obj.id,
+                       _external=True)
